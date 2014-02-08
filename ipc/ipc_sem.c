590321d6c323818e2cac1d6b9dbf6731316fe1a9
lihones <lihones@gmail.com>
2014-02-08 10:25:41 +0800

    commit

--- ipc/ipc_sem.c
+++ ipc/ipc_sem.c
@@ -11,15 +11,7 @@
 
 void sem_main()
 {
-	char* file_path = "./text";
-	int fd = open(file_path, O_RDWR | O_CREAT);
-	int out_u = dup(STDOUT_U);
-	dup2(fd, STDOUT_U);
-	close(fd);
-	printf("hello\n");
-	fflush(stdout);
-	dup2(out_u, STDOUT_U);
-	printf("hello\n");
+
 }
 
 int main(int argc, char* argv[])
