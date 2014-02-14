43016797004988975f6679b5521f1b3f4b4df166
lihones <lihones@gmail.com>
2014-02-14 11:58:40 +0800

    commit

--- primary/xwindow/x_main.c
+++ primary/xwindow/x_main.c
@@ -1,60 +1,77 @@
 /*
-* Simple Xlib application drawing a box in a window.
-*/
+ Simple Xlib application drawing a box in a window.
+ gcc input.c -o output -lX11
+ */
 
 #include <X11/Xlib.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 int x_main(int argc, char* argv[])
 {
-        struct _XDisplay *d;
-        int s;
-        Window w;
-        XEvent e;
+	Display *d;
+	Window w;
+	XEvent e;
+	char *msg = "Hello, World!";
+	int s;
 
-        /* open connection with the server */
-        d = XOpenDisplay(NULL);
-        if(d == NULL)
-        {
-                printf("Cannot open display\n");
-                exit(1);
-        }
-        s = DefaultScreen(d);
+	bool done = false;
 
-        /* create window */
-        w = XCreateSimpleWindow(d,
-                                RootWindow(d, s),
-                                10,
-                                10,
-                                100,
-                                100,
-                                1,
-                                BlackPixel(d, s),
-                                WhitePixel(d, s));
+	/* open connection with the server */
+	d = XOpenDisplay(NULL );
+	if (d == NULL ) {
+		fprintf(stderr, "Cannot open display\n");
+		exit(1);
+	}
 
-        /* select kind of events we are interested in */
-        XSelectInput(d, w, ExposureMask | KeyPressMask);
+	s = DefaultScreen(d);
 
-        /* map (show) the window */
-        XMapWindow(d, w);
+	/* create window */
+	w = XCreateSimpleWindow(d, RootWindow(d, s) , 10, 10, 640, 480, 0,
+	BlackPixel(d, s), WhitePixel(d, s));
 
-        /* event loop */
-        while(1)
-        {
-                XNextEvent(d, &e);
-                /* draw or redraw the window */
-                if(e.type == Expose)
-                {
-                        XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10);
-                }
-                /* exit on key press */
-                if(e.type == KeyPress)
-                        break;
-        }
+	/* register interest in the delete window message */
+	Atom wmDeleteMessage = XInternAtom(d, "WM_DELETE_WINDOW", False);
+	XSetWMProtocols(d, w, &wmDeleteMessage, 1);
 
-        /* close connection to server */
-        XCloseDisplay(d);
+	/* select kind of events we are interested in */
+	XSelectInput(d, w, ExposureMask | KeyPressMask | StructureNotifyMask);
 
-        return 0;
+	/* map (show) the window */
+	XMapWindow(d, w);
+
+	/* event loop */
+	while (!done) {
+		XNextEvent(d, &e);
+		/* draw or redraw the window */
+		if (e.type == Expose) {
+			XFillRectangle(d, w, DefaultGC(d, s) , 20, 20, 10, 10);
+			XDrawString(d, w, DefaultGC(d, s), 50, 50, msg, strlen(msg));
+		}
+
+			/* exit on key press */
+		switch (e.type) {
+
+		case KeyPress:
+			XDestroyWindow(d, w);
+			break;
+
+		case DestroyNotify:
+			done = true;
+			break;
+
+		case ClientMessage:
+			if (e.xclient.data.l[0] == wmDeleteMessage) {
+				done = true;
+			}
+			break;
+		}
+	}
+
+	/* close connection to server */
+	XCloseDisplay(d);
+
+	return 0;
 }
