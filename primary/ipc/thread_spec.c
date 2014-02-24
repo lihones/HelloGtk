3c156cf466e81c9166d9e8220651f15859190482
lihones <lihones@gmail.com>
2014-02-24 15:40:09 +0800

    commit

---
+++ primary/ipc/thread_spec.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+pthread_key_t key;
+
+void print_ids(char *who)
+{
+	printf("%s: pid-%x, tid-%x\n", who, getpid(), pthread_self());
+}
+
+void* thread_func_a(void *arg)
+{
+	print_ids("thread_a");
+	int tid = pthread_self();
+	pthread_setspecific(key, (void*)(&tid));
+	sleep(1);
+	int *spec = (int*)(pthread_getspecific(key));
+	printf("thread_a get spec: %x\n", *spec);
+	return NULL;
+}
+
+void* thread_func_b(void *arg)
+{
+	print_ids("thread_b");
+	int tid = pthread_self();
+	pthread_setspecific(key, (void*)(&tid));
+	int *spec = (int*)(pthread_getspecific(key));
+	printf("thread_b get spec: %x\n", *spec);
+	return NULL;
+}
+
+void thread_mutex_main()
+{
+	print_ids("main thread");
+
+	pthread_key_create(&key, NULL);
+
+	pthread_t ta;
+	pthread_create(&ta, NULL, thread_func_a, NULL);
+	pthread_t tb;
+	pthread_create(&tb, NULL, thread_func_b, NULL);
+
+	pthread_join(ta, NULL);
+	pthread_join(tb, NULL);
+
+	pthread_key_delete(key);
+}
+
+int main(int argc, char* argv[])
+{
+	thread_mutex_main();
+
+	return 0;
+}
